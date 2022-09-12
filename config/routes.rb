4dbafecb96eb057eda47f03769937cd1d8b7f646
jmoseka <jamylam3@gmail.com>
2022-09-12 23:05:11 +0300

    Add default index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  # get 'greetings/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "greetings#index"
 end

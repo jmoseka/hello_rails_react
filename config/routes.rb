eb50e38e4591d4593ce8541c3c20c358608e9971
jmoseka <jamylam3@gmail.com>
2022-09-13 20:16:28 +0300

    Add api greetings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # namespace :api do
+  #   namespace :v1 do
+  #     get 'greetings/index'
+  #     get 'greetings/show'
+  #   end
+  # end
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get 'greetings', to: 'greetings#index'
+    end
+  end
   # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
